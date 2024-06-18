6caadbe81c9b39b79d0a4321fa67a92dfd126c33
Nikbraeu <niklas.braeu@gmx.net>
2024-06-18 14:52:39 +0200

    Add files via upload

---
+++ Programme/Book_Manager.py
@@ -0,0 +1,16 @@
+#Bücher hinzufügen
+
+def book_add(inventory, title, author, price):
+    new_book = {
+        'Title' : title,
+        'Author' : author,
+        'Price' : price,
+    }
+    inventory.append(new_book)
+
+
+inventory[]
+
+book_add(inventory, "Timos Dirty Mind", "Niklas" ,"1" )
+
+print(inventory)
